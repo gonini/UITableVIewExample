94fcdbf555d5812bb6f019c4a24cbba47610aea5
GongUi <zhanggoni@gmail.com>
2019-05-28 16:28:33 +0900

    초기 설정 작업
    Kingfisher 추가

---
+++ Pods/Target Support Files/Pods-UITableVIewExample/Pods-UITableVIewExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UITableVIewExample : NSObject
+@end
+@implementation PodsDummy_Pods_UITableVIewExample
+@end
---
+++ UITableVIewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableVIewExample
+//
+//  Created by 장공의 on 27/05/2019.
+//  Copyright © 2019 gonini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
